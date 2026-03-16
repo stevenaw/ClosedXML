e76bdaf6658819607ae7e4bfa1b5a01d4893e56a
stevenaw <sweerdenburg@gmail.com>
2026-03-16 16:26:42 -0400

    Workaround issue in NUnit 4.5

--- ClosedXML.Tests/Excel/InsertData/InsertDataReaderFactoryTests.cs
+++ ClosedXML.Tests/Excel/InsertData/InsertDataReaderFactoryTests.cs
@@ -59,7 +59,7 @@ namespace ClosedXML.Tests.Excel.InsertData
         }
 
         [TestCaseSource(nameof(ArraySources))]
-        public void CanCreateArrayReader<T>(IEnumerable<T> data)
+        public void CanCreateArrayReader(IEnumerable<object> data)
         {
             var reader = InsertDataReaderFactory.Instance.CreateReader(data);
 
